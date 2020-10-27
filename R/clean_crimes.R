5a80bffa8508eb252155b4d168dfa8f9213405b9
erikbjohn <erikbjohn@gmail.com>
2020-10-27 15:20:34 -0500

    Moved data to box, automated the crime import for 2016-2017

---
+++ R/clean_crimes.R
@@ -0,0 +1,8 @@
+clean_crimes <- function(file_name){
+ # dt <- fread(file_name, fill=TRUE)
+  dt <- as.data.table(read.csv2(file_name, header = TRUE, sep=','))
+  setnames(dt, names(dt), stringr::str_to_lower(stringr::str_replace_all(names(dt), '\\.', '_')))
+  setnames(dt, names(dt), stringr::str_replace(names(dt), pattern = '\\_$', ''))
+  dt <- dt[!(case_number %in% '')]
+  return(dt)
+}
--- R/get_crime_data.R
+++ R/get_crime_data.R
@@ -1,8 +1,16 @@
 get_crime_data <- function(){
-  crime_data_location <- '../data/clean/crime.rds'
+  crime_data_location <- '~/Dropbox/pkg.data/flood_risk/clean/crime.rds'
   if(!file.exists(crime_data_location)){
-    file_list <- list.files('data/raw/crime', recursive = TRUE, pattern='.txt', full.names = TRUE)
-    l_files <- lapply(file_list, fread)
+    source('R/clean_crimes.R')
+    file_list <- list.files('~/Dropbox/pkg.data/flood_risk/raw/crime', recursive = TRUE, full.names = TRUE)
+    l_crimes <- vector(mode='list', length=length(file_list))
+    for(i_file_name in 1:length(file_list)){
+      file_name <- file_list[i_file_name]
+      cat(i_file_name, file_name, '\n')
+      l_crimes[[i_file_name]] <- clean_crimes(file_name)
+    }
+    dt <- data.table::rbindlist(l_crimes, use.name=TRUE, fill=TRUE)
+    
     # OH MY, SO MUCH CLEANING!
     # TEST THE SLACK INTEGRATION
   }
