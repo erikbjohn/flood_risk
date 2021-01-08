035c9d6d7d11b65de597d88f44818fe193575d53
erikbjohn <erikbjohn@gmail.com>
2021-01-08 11:50:30 -0600

    added fread as csv2 alternative

--- R/clean_crimes.R
+++ R/clean_crimes.R
@@ -1,6 +1,9 @@
 clean_crimes <- function(file_name){
  # dt <- fread(file_name, fill=TRUE)
-  dt <- as.data.table(read.csv2(file_name, header = TRUE, sep=','))
+  dt <- as.data.table(read.csv2(file_name, header = TRUE, sep=',', stringsAsFactors = FALSE))
+  if(length(names(dt))==1){
+    dt <- fread(file_name, header=TRUE)
+  }
   setnames(dt, names(dt), stringr::str_to_lower(stringr::str_replace_all(names(dt), '\\.', '_')))
   setnames(dt, names(dt), stringr::str_replace(names(dt), pattern = '\\_$', ''))
   dt <- dt[!(case_number %in% '')]
